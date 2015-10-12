14bd4cb1aa2b6f78da39d4faa6861f3cca65725b
slymnkbdyi <suleyman.kabadayi@bil.omu.edu.tr>
2015-10-12 17:10:26 +0300

    Mvc İle Web Site
    Mvc ile Web Site

---
+++ ilksitem/Controllers/CozumlerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ilksitem.Controllers
+{
+    public class CozumlerController : Controller
+    {
+        // GET: Cozumler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
